5502f94a594dad7eb51b360115c1ad7b77c4dd9c
NahuelSandoval <apos_facu@hotmail.com>
2023-02-20 20:23:59 -0300

    magia

--- src/componentes/personajes.js
+++ src/componentes/personajes.js
@@ -1,27 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
 
+
 const Personajes = () => {
 
     const [personajes, setPersonajes] = useState([]);
+    const [estaCargando, setEstaCargando] = useState(true)
 
     useEffect(() => {
         fetch("https://hp-api.onrender.com/api/characters")
             .then((response) => response.json())
-            .then((personajes) => setPersonajes(personajes))
+            .then((personajes) => {
+                setPersonajes(personajes);
+                setEstaCargando(false);
+            });
+                
 }
     );
+if (estaCargando){
+return (
+    <div> 
+        <h2>Cargando personajes ....</h2>
+    </div>
+);
 
+}
     return (
         <>
-            <div>personajes</div>
+            <h2>Personajes</h2>
             <Container>
                 {personajes ?
                     <Row>
                         {personajes.map((personaje, index) => (
                             <Col sm={4} key={index}>
                                 <img style={{ width: "80%" }} src={personaje.image} alt="personaje" />
-                                <h3>{personaje.name}</h3>
+                                <h2>{personaje.name}</h2>
                                 <p>{personaje.house}</p>
                             </Col>
                         )

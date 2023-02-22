c17960df3352c161c4af6c9ad51182191847f5ce
NahuelSandoval <apos_facu@hotmail.com>
2023-02-22 16:40:05 -0300

    casi renovando

--- src/componentes/TiendaNueva/data.js
+++ src/componentes/TiendaNueva/data.js
@@ -1,38 +1,67 @@
+import buzo from '../productos/tienda/buzo.png';
+import campera from '../productos/tienda/campera.jpg';
+import conjunto from '../productos/tienda/conjunto.jfif';
+import gorro from '../productos/tienda/gorro.jfif';
+import taza from '../productos/tienda/taza.jfif';
+import bufanda from '../productos/tienda/bufanda.jfif';
+import llavero from '../productos/tienda/llavero.jfif';
+import chomba from '../productos/tienda/chomba.jfif'; 
 
 export const data = [
 	{
 		id: 1,
-		img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-		nameProduct: 'Zapatos Nike',
-		price: 80,
+		img: buzo,
+		nameProduct: 'Buzo',
+		price: 1500,
 		quantity: 1,
 	},
 	{
 		id: 2,
-		img: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80',
-		nameProduct: 'Reloj',
-		price: 50,
+		img: campera,
+		nameProduct: 'Campera',
+		price: 2200,
 		quantity: 1,
 	},
 	{
 		id: 3,
-		img: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
-		nameProduct: 'Smartwatch',
-		price: 90,
+		img: conjunto,
+		nameProduct: 'Conjunto',
+		price: 3800,
 		quantity: 1,
 	},
 	{
 		id: 4,
-		img: 'https://images.unsplash.com/photo-1585386959984-a4155224a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-		nameProduct: 'Perfume',
-		price: 50,
+		img: gorro,
+		nameProduct: 'Gorro',
+		price: 800,
 		quantity: 1,
 	},
 	{
 		id: 5,
-		img: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-		nameProduct: 'Audifonos',
-		price: 20,
+		img: taza,
+		nameProduct: 'Taza',
+		price: 450,
+		quantity: 1,
+	},
+	{
+		id: 6,
+		img: bufanda,
+		nameProduct: 'Bufanda',
+		price: 800,
+		quantity: 1,
+	},
+	{
+		id: 7,
+		img: llavero,
+		nameProduct: 'Llavero',
+		price: 300,
+		quantity: 1,
+	},
+	{
+		id: 8,
+		img: chomba,
+		nameProduct: 'Chomba',
+		price: 1500,
 		quantity: 1,
 	},
 	// Aquí pueden agregar más o modificar los productos existentes
